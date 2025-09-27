92504da91973073f2c8ce299f0fd565e542d0116
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-09-27 14:47:46 -0300

    3. Logs

---
+++ 0x04/log/LoggerInFile.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import java.util.logging.FileHandler;
+
+import java.util.logging.Logger;
+
+import java.util.logging.SimpleFormatter;
+
+public class LoggerInFile {
+
+    public static void main(String[] args) throws IOException {
+
+        Logger logger = Logger.getLogger("MyLog");
+
+        FileHandler fileHandler;
+
+        fileHandler = new FileHandler("logs.txt");
+
+        logger.addHandler(fileHandler);
+
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+
+        fileHandler.setFormatter(simpleFormatter);
+
+        logger.info("teste de Log");
+
+        logger.info("Olá, eu sou o teste da classe principal");
+
+    }
+
+}
